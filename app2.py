067a340f13986352b328e3a46b14493b25052a88
INGQRAUDIO <quirozing117@gmail.com>
2026-07-01 00:05:09 -0600

    Add files via upload

--- app2.py
+++ app2.py
@@ -1011,7 +1011,7 @@ if participante_seleccionado:
 
 
 
-    col_a, col_b, col_c = st.columns([1, 2, 1])
+    col_a, col_b, col_c = st.columns([1, 3, 1])
     with col_b:
         modo_vista = st.radio(
             "Vista del bracket",
